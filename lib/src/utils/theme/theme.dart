c948d676ce7ccfdc13891b7829d29ff905590c3f
Athaya Hanif <96409853+DannSama@users.noreply.github.com>
2023-11-29 11:48:06 +0700

    layla
    ternyata buat baru

---
+++ lib/src/utils/theme/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TAppTheme {
+  static ThemeData lightTheme = ThemeData(
+      brightness: Brightness.light,
+      textTheme: TextTheme(
+        headline2: GoogleFonts.montserrat(color : Colors.black),
+      )
+  );
+
+
+  static ThemeData darkTheme = ThemeData();
+  
+}
